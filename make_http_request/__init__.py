9281d10212ca499194bdd79af42c1028d5d83e2f
John Taylor <jftuga@users.noreply.github.com>
2024-02-09 08:46:31 -0500

    version bump

--- make_http_request/__init__.py
+++ make_http_request/__init__.py
@@ -4,7 +4,7 @@ class make_http_request():
     __title__ = "make_http_request"
     __description__ = "HTTP requester with retry"
     __url__ = "https://github.com/jftuga/python"
-    __version__ = "0.1.0"
+    __version__ = "0.1.1"
     
     def __init__(self, url):
         self.response = requests.get(url)

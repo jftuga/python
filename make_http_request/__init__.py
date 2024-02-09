ea85d1edd2df9b76cf4a642b2bbd7ac24b3f76cd
John Taylor <jftuga@users.noreply.github.com>
2024-02-09 08:05:19 -0500

    change scoping

--- make_http_request/__init__.py
+++ make_http_request/__init__.py
@@ -1,10 +1,10 @@
 import requests
 
-__title__ = "make_http_request"
-__description__ = "HTTP requester with retry"
-__url__ = "https://github.com/jftuga/python"
-__version__ = "0.1.0"
-
 class make_http_request():
+    __title__ = "make_http_request"
+    __description__ = "HTTP requester with retry"
+    __url__ = "https://github.com/jftuga/python"
+    __version__ = "0.1.0"
+    
     def __init__(self, url):
         self.response = requests.get(url)
